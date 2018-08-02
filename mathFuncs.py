96391b9ae6d4d1e26072a43764f6e872737a094f
Alon Hershenhorn <hershen@phas.ubc.ca>
2018-08-02 11:49:16 -0700

    1)Added radial_xyz, phi_xyz. 2) cosTheta uses radial_xyz, 3) autopep8

--- mathFuncs.py
+++ mathFuncs.py
@@ -1,43 +1,62 @@
 #!/usr/bin/python3
 import numpy as np
 
+
 def vectorDot(v1, v2):
     """Return dot product of vectors v1 and v2
 
     Different from np.dot, np.inner in that if the input is an array of arrays, returns the right array."""
 
-    return np.sum(np.multiply(v1,v2)) if isinstance(v1, list) or v1.ndim < 2 else np.sum(np.multiply(v1,v2), axis=1)
+    return np.sum(np.multiply(v1, v2)) if isinstance(v1, list) or v1.ndim < 2 else np.sum(np.multiply(v1, v2), axis=1)
 
-def cosTheta(x,y,z):
+
+def radial_xyz(x, y, z):
+    """ Return radial from x,y,z coordinates """
+    return np.sqrt(x ** 2 + y ** 2 + z ** 2)
+
+
+def phi_xyz(x, y, z):
+    """Return spherical phi coordinate from x,y,z cartezian coordinates"""
+    return np.arctan2(y, x)
+
+
+def cosTheta(x, y, z):
     """ Returns the cos(theta) of the cartezian vector (x,y,z) in radians"""
-    return z / np.sqrt(x**2 + y**2 + z**2)
+    return z / radial_xyz(x, y, z)
+
 
 def angleBetween(v1, v2):
     """ Returns the angle in radians between vectors v1 and v2 """
 
-    v1norms = np.linalg.norm(v1) if isinstance(v1, list) or v1.ndim < 2 else np.linalg.norm(v1, axis = 1)
-    v2norms = np.linalg.norm(v2) if isinstance(v2, list) or v2.ndim < 2 else np.linalg.norm(v2, axis = 1)
+    v1norms = np.linalg.norm(v1) if isinstance(v1, list) or v1.ndim < 2 else np.linalg.norm(v1, axis=1)
+    v2norms = np.linalg.norm(v2) if isinstance(v2, list) or v2.ndim < 2 else np.linalg.norm(v2, axis=1)
+
+    # comment
 
     return np.arccos(vectorDot(v1, v2) / v1norms / v2norms)
+
 
 def lorentzDot(v1, v2):
     """ Perform 4 vector dot product
     Assume time like value at end"""
 
-    multiplied = np.multiply(v1,v2)
+    multiplied = np.multiply(v1, v2)
 
     if isinstance(v1, list) or v1.ndim < 2:
         return multiplied[3] - np.sum(multiplied[:3])
 
     return multiplied[:, 3] - multiplied[:, 0] - multiplied[:, 1] - multiplied[:, 2]
 
+
 def mass2(lorentzVector):
     """Get mass of lorentz vector squared"""
     return lorentzDot(lorentzVector, lorentzVector)
 
+
 def mass(lorentzVector):
     """Get mass of lorentz vecot"""
     return np.sqrt(mass2(lorentzVector))
+
 
 def cosHelicity1(grandParent, parent, daughter):
     """ Calculate cosine helicity of the daughter.
@@ -45,7 +64,11 @@ def cosHelicity1(grandParent, parent, daughter):
     Taken from BAD522 v6, page 120, eq. 141
     grandparent, parent and daughter are P, Q, D in that equation
     """
-    return (lorentzDot(grandParent,daughter)*mass2(parent) - lorentzDot(grandParent,parent)*lorentzDot(parent,daughter))/np.sqrt((lorentzDot(grandParent,parent)**2 - mass2(parent)*mass2(grandParent))*(lorentzDot(parent,daughter)**2 - mass2(parent)*mass2(daughter)))
+    return (lorentzDot(grandParent, daughter) * mass2(parent) - lorentzDot(grandParent, parent) * lorentzDot(parent,
+                                                                                                             daughter)) / np.sqrt(
+        (lorentzDot(grandParent, parent) ** 2 - mass2(parent) * mass2(grandParent)) * (
+                lorentzDot(parent, daughter) ** 2 - mass2(parent) * mass2(daughter)))
+
 
 def effError(nom, denom):
     """Calculate efficiency error using binomial distribution
@@ -55,6 +78,6 @@ def effError(nom, denom):
     Can be extended to include edge cases with CDF note 5894"""
 
     nom, denom = np.asarray(nom, dtype=float), np.asarray(denom)
-    eff = nom/denom
+    eff = nom / denom
 
-    return np.sqrt(eff*(1-eff)/denom)
+    return np.sqrt(eff * (1 - eff) / denom)
--- test_mathFuncs.py
+++ test_mathFuncs.py
@@ -4,74 +4,77 @@ import math
 import numpy as np
 import pytest
 
+
 def test_vectorDot():
+    # All zeros
+    assert mathFuncs.vectorDot([0, 0, 0], [0, 0, 0]) == 0
 
-    #All zeros
-    assert mathFuncs.vectorDot([0,0,0], [0,0,0]) == 0
+    # nans
+    assert np.isnan(mathFuncs.vectorDot([np.nan, 0, 0], [0, 0, 0]))
+    assert np.isnan(mathFuncs.vectorDot([0, np.nan, 0], [0, 0, 0]))
+    assert np.isnan(mathFuncs.vectorDot([0, 0, np.nan], [0, 0, 0]))
+    assert np.isnan(mathFuncs.vectorDot([0, 0, 0], [np.nan, 0, 0]))
+    assert np.isnan(mathFuncs.vectorDot([0, 0, 0], [0, np.nan, 0]))
+    assert np.isnan(mathFuncs.vectorDot([0, 0, 0], [0, 0, np.nan]))
 
-    #nans
-    assert np.isnan(mathFuncs.vectorDot([np.nan,0,0], [0,0,0]))
-    assert np.isnan(mathFuncs.vectorDot([0, np.nan ,0], [0,0,0]))
-    assert np.isnan(mathFuncs.vectorDot([0,0, np.nan], [0,0,0]))
-    assert np.isnan(mathFuncs.vectorDot([0,0,0], [np.nan,0,0]))
-    assert np.isnan(mathFuncs.vectorDot([0,0,0], [0,np.nan, 0]))
-    assert np.isnan(mathFuncs.vectorDot([0,0,0], [0, 0, np.nan]))
+    assert mathFuncs.vectorDot([1, 2, 3], [4, 5, 6]) == 32
 
+    v1 = np.array([[1, 2, 3], [1, 2, 3]])
+    v2 = np.array([[1, 2, 3], [4, 5, 6]])
 
-    assert mathFuncs.vectorDot([1,2,3], [4,5,6]) == 32
+    assert (mathFuncs.vectorDot(v1, v2) == np.array([14, 32])).all()
 
-    v1 = np.array([[1,2,3],[1,2,3]])
-    v2 = np.array([[1,2,3],[4,5,6]])
-
-    assert (mathFuncs.vectorDot(v1,v2) == np.array([14, 32])).all()
 
 def test_cosTheta():
-    #All non zeroes
-    assert mathFuncs.cosTheta(1,2,3) == 3 / math.sqrt(1+4+9)
+    # All non zeroes
+    assert mathFuncs.cosTheta(1, 2, 3) == 3 / math.sqrt(1 + 4 + 9)
 
-    #x zero
-    assert mathFuncs.cosTheta(0,2,3) == 3 / math.sqrt(0+4+9)
-    #y zero
-    assert mathFuncs.cosTheta(1,0,3) == 3 / math.sqrt(1+0+9)
-    #z zero
-    assert mathFuncs.cosTheta(1,2,0) == 0 / math.sqrt(1+4+9)
+    # x zero
+    assert mathFuncs.cosTheta(0, 2, 3) == 3 / math.sqrt(0 + 4 + 9)
+    # y zero
+    assert mathFuncs.cosTheta(1, 0, 3) == 3 / math.sqrt(1 + 0 + 9)
+    # z zero
+    assert mathFuncs.cosTheta(1, 2, 0) == 0 / math.sqrt(1 + 4 + 9)
 
-    #x nan
-    assert np.isnan(mathFuncs.cosTheta(np.nan,2,3))
-    #y zero
-    assert np.isnan(mathFuncs.cosTheta(1, np.nan,3))
-    #z zero
+    # x nan
+    assert np.isnan(mathFuncs.cosTheta(np.nan, 2, 3))
+    # y zero
+    assert np.isnan(mathFuncs.cosTheta(1, np.nan, 3))
+    # z zero
     assert np.isnan(mathFuncs.cosTheta(1, 2, np.nan))
 
-    #all zero - divistion by 0 warning
+    # all zero - divistion by 0 warning
     with pytest.warns(RuntimeWarning, match='invalid value encountered in double_scalars'):
-        assert np.isnan(mathFuncs.cosTheta(0,0,0))
+        assert np.isnan(mathFuncs.cosTheta(0, 0, 0))
 
 
 def test_angleBetween():
-    v1 = np.array([1,0, 0])
-    v2 = np.array([1,0, 0])
+    v1 = np.array([1, 0, 0])
+    v2 = np.array([1, 0, 0])
 
     assert mathFuncs.angleBetween(v1, v2) == 0
     assert mathFuncs.angleBetween(v1, -v2) == np.pi
     assert mathFuncs.angleBetween(-v1, -v2) == 0
 
-    assert mathFuncs.angleBetween([1,2,3], [4,5,6]) == math.acos((4+10+18) / math.sqrt(1 + 4 +9) / math.sqrt(16 + 25 + 36))
+    assert mathFuncs.angleBetween([1, 2, 3], [4, 5, 6]) == math.acos(
+        (4 + 10 + 18) / math.sqrt(1 + 4 + 9) / math.sqrt(16 + 25 + 36))
 
     with pytest.warns(RuntimeWarning, match='invalid value encountered in divide'):
-        assert np.isnan(mathFuncs.angleBetween([0,0,0], [0,0,0]))
+        assert np.isnan(mathFuncs.angleBetween([0, 0, 0], [0, 0, 0]))
 
-    assert np.isnan(mathFuncs.angleBetween([np.nan,0,0], [0,0,0]))
-    assert np.isnan(mathFuncs.angleBetween([0, np.nan ,0], [0,0,0]))
-    assert np.isnan(mathFuncs.angleBetween([0,0, np.nan], [0,0,0]))
-    assert np.isnan(mathFuncs.angleBetween([0,0,0], [np.nan,0,0]))
-    assert np.isnan(mathFuncs.angleBetween([0,0,0], [0,np.nan, 0]))
-    assert np.isnan(mathFuncs.angleBetween([0,0,0], [0, 0, np.nan]))
+    assert np.isnan(mathFuncs.angleBetween([np.nan, 0, 0], [0, 0, 0]))
+    assert np.isnan(mathFuncs.angleBetween([0, np.nan, 0], [0, 0, 0]))
+    assert np.isnan(mathFuncs.angleBetween([0, 0, np.nan], [0, 0, 0]))
+    assert np.isnan(mathFuncs.angleBetween([0, 0, 0], [np.nan, 0, 0]))
+    assert np.isnan(mathFuncs.angleBetween([0, 0, 0], [0, np.nan, 0]))
+    assert np.isnan(mathFuncs.angleBetween([0, 0, 0], [0, 0, np.nan]))
 
-    v1 = np.array([[1,2,3],[1,2,3]])
-    v2 = np.array([[1,2,3],[4,5,6]])
+    v1 = np.array([[1, 2, 3], [1, 2, 3]])
+    v2 = np.array([[1, 2, 3], [4, 5, 6]])
 
-    assert (mathFuncs.angleBetween(v1, v2) == np.array([0,math.acos((4+10+18) / math.sqrt(1 + 4 +9) / math.sqrt(16 + 25 + 36))])).all()
+    assert (mathFuncs.angleBetween(v1, v2) == np.array(
+        [0, math.acos((4 + 10 + 18) / math.sqrt(1 + 4 + 9) / math.sqrt(16 + 25 + 36))])).all()
+
 
 def test_lorentzDot():
     v1 = [1, 2, 3, 4]
@@ -80,27 +83,56 @@ def test_lorentzDot():
     assert mathFuncs.lorentzDot(v1, v2) == -6
     assert mathFuncs.lorentzDot(-np.array(v1), v2) == 6
 
-    assert mathFuncs.lorentzDot([1,2,3,4], [0,0,0,0]) == 0
+    assert mathFuncs.lorentzDot([1, 2, 3, 4], [0, 0, 0, 0]) == 0
 
-    assert np.isnan(mathFuncs.lorentzDot([np.nan,1, 2, 3], [5, 6, 7, 8]))
+    assert np.isnan(mathFuncs.lorentzDot([np.nan, 1, 2, 3], [5, 6, 7, 8]))
 
-    v1 = np.array([[1,2,3, 4], [1, 2, 3, 5]])
-    v2 = np.array([[1,2,3, 4], [5, 6, 7, 8]])
+    v1 = np.array([[1, 2, 3, 4], [1, 2, 3, 5]])
+    v2 = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
     assert (mathFuncs.lorentzDot(v1, v2) == np.array([2, 2])).all()
 
+
 def test_mass():
+    assert mathFuncs.mass([1, 2, 3, 10]) == np.sqrt(86)
+    assert mathFuncs.mass([0, 0, 0, 0]) == 0
 
-    assert mathFuncs.mass([1,2,3,10]) == np.sqrt(86)
-    assert mathFuncs.mass([0,0,0,0]) == 0
-
-    v = np.array([[0,0,0,0], [1,2,3,10], [1,2,3,np.sqrt(14)]])
+    v = np.array([[0, 0, 0, 0], [1, 2, 3, 10], [1, 2, 3, np.sqrt(14)]])
     assert (mathFuncs.mass(v) == np.array([0, np.sqrt(86), 0])).all()
 
-def test_effError():
-    assert mathFuncs.effError(0.5,1) == 0.5
-    assert np.allclose(mathFuncs.effError(np.array([8,5]), np.array([10,10])) , np.array([math.sqrt(0.8*0.2/10), math.sqrt(0.5*0.5/10)]) )
 
-    #These 2 are not strictly correct. They're correct for the current formula
-    assert mathFuncs.effError(0,1) == 0
-    assert mathFuncs.effError(1,1) == 0
+def test_effError():
+    assert mathFuncs.effError(0.5, 1) == 0.5
+    assert np.allclose(mathFuncs.effError(np.array([8, 5]), np.array([10, 10])),
+                       np.array([math.sqrt(0.8 * 0.2 / 10), math.sqrt(0.5 * 0.5 / 10)]))
+
+    # These 2 are not strictly correct. They're correct for the current formula
+    assert mathFuncs.effError(0, 1) == 0
+    assert mathFuncs.effError(1, 1) == 0
+
+
+def test_radial_xyz():
+    # All non zeroes
+    assert mathFuncs.radial_xyz(1, 2, 3) == math.sqrt(14)
+
+    # x zero
+    assert mathFuncs.radial_xyz(0, 2, 3) == math.sqrt(13)
+
+    assert mathFuncs.radial_xyz(0, 0, 0) == 0
+
+    # x nan
+    assert np.isnan(mathFuncs.radial_xyz(np.nan, 2, 3))
+
+
+def test_phi_xyz():
+    assert mathFuncs.phi_xyz(1, 2, 3) == 1.107148717794090408972
+
+    assert mathFuncs.phi_xyz(0, 2, 3) == 1.570796326794896557999
+
+    assert mathFuncs.phi_xyz(1, 0, 3) == 0
+
+    assert (mathFuncs.phi_xyz([1, 0, 1], [2, 2, 0], [3, 3, 3]) == [1.107148717794090408972, 1.570796326794896557999,
+                                                                   0]).all()
+
+    # x nan
+    assert np.isnan(mathFuncs.phi_xyz(np.nan, 2, 3))
