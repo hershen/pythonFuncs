40ad47be61819b891a91d4ac20c010f0cb01e3d0
Alon Hershenhorn <hershen@phas.ubc.ca>
2018-08-02 11:47:39 -0700

    run autopep8

--- alpFuncs.py
+++ alpFuncs.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 import re
 
+
 def getMass(filename):
     """Return mass from the filename"""
 
-    #remove up to 'mass'
+    # remove up to 'mass'
     filename = filename[filename.find('mass') + 4:]
 
     return filename[:filename.find('_')]
 
+
 def getRun(filename):
     """Return Run from the filename"""
 
-    #string after run
+    # string after run
     postRunLoc = filename.find('.01.')
 
-    #remove up to 'mass'
-    preRunLoc = re.search('_.-', filename).end() + 3 #+3 for either 'Run' or 'Ups'
+    # remove up to 'mass'
+    preRunLoc = re.search('_.-', filename).end() + 3  # +3 for either 'Run' or 'Ups'
 
     return filename[preRunLoc:postRunLoc]
--- myPlotting.py
+++ myPlotting.py
@@ -2,38 +2,42 @@
 import pickle as pl
 import os
 
+
 def getHistBinEdges_topsFromAxes(axes):
-    #list of (x,y) coordinates of each vertex of histograms.
+    # list of (x,y) coordinates of each vertex of histograms.
     xy = axes.patches[0].get_xy()
 
-    #Take only vertical lines (contain all information)
+    # Take only vertical lines (contain all information)
     data = xy[::2]
 
-    #bin edges are first column.
-    binEdges = data[:,0]
+    # bin edges are first column.
+    binEdges = data[:, 0]
 
-    #tops can disregard first entry (probably at 0)
-    tops = data[1:,1]
+    # tops can disregard first entry (probably at 0)
+    tops = data[1:, 1]
 
     return binEdges, tops
+
 
 def _saveFigFlat(fig, filename):
     """ filename should contain extension"""
     fig.savefig('figureDump/{}'.format(filename), dpi=300)
 
+
 def _saveFigPickled(fig, filename):
     """ filename should contain extension"""
     pl.dump(fig, open('figureDump/{}'.format(filename), 'wb'))
+
 
 def saveFig(fig, filename):
     """ Save a pyplot figure """
     filename, ext = os.path.splitext(filename)
 
-    #Create figureDump dir
+    # Create figureDump dir
     if not os.path.isdir('figureDump'):
         os.mkdir('figureDump')
 
-    #If extension included, save only thatfiletype
+    # If extension included, save only thatfiletype
     if ext:
         if ext == '.pl':
             _saveFigPickled(fig, filename + ext)
@@ -41,7 +45,7 @@ def saveFig(fig, filename):
             _saveFigFlat(fig, filename + ext)
         return
 
-    #otherwise, save multiple filetypes
+    # otherwise, save multiple filetypes
     _saveFigFlat(fig, filename + '.png')
     _saveFigFlat(fig, filename + '.pdf')
     # _saveFigPickled(fig, filename + '.pl')
