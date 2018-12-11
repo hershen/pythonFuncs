7060b9b95d97c709b9c75766dbf5587f1fbaf546
Alon Hershenhorn <hershen@gmail.com>
2018-12-11 10:24:26 -0800

    Add getHdfGroups

--- generalFuncs.py
+++ generalFuncs.py
@@ -4,3 +4,8 @@ import pandas as pd
 def getCsv(filename):
     with open(filename, 'r') as SP1074csv:
         return pd.read_csv(SP1074csv)
+
+
+def getHdfGroups(filename):
+    with pd.HDFStore(filename) as file:
+        return list(file.keys())
