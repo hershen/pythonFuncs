e863b35a9bdbae62b459888d3f074b6a81a8c166
Alon Hershenhorn <hershen@gmail.com>
2019-01-21 13:13:51 -0800

    If missing file in getHdfGroups, raise

--- generalFuncs.py
+++ generalFuncs.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+import errno
+import os
+
 import pandas as pd
+
 
 def getCsv(filename):
     with open(filename, 'r') as SP1074csv:
@@ -7,5 +11,8 @@ def getCsv(filename):
 
 
 def getHdfGroups(filename):
+    if not os.path.isfile(filename):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
+
     with pd.HDFStore(filename) as file:
         return list(file.keys())
