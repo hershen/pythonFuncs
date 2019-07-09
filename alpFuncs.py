0e052e3d4cc26d462554184aed09fc6e08779399
Alon Hershenhorn <hershen@gmail.com>
2019-07-09 15:44:31 -0700

    Add SP2400 scale to data

--- alpFuncs.py
+++ alpFuncs.py
@@ -23,6 +23,10 @@ SCALE_SP5797_TO_DATA = {'Y2S_OffPeak': 0.212249235772358,
                         'Y4S_OffPeak': 0.678979604596849,
                         'Y4S_OnPeak': 0.656229936094144}
 
+SCALE_SP2400_TO_DATA = {'Y2S_OnPeak': 4.92927027672283,
+                        'Y3S_OnPeak': 4.20954351890712,
+                        'Y4S_OnPeak': 26.3976194064715}
+
 
 def getMass(filename):
     """Return mass from the filename"""
